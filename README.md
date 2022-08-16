c6dd246c0e97217546944fb22309a28499b12fed
Miriyam Bird <miriyam_bird@icloud.com>
2022-08-16 16:28:53 +0300

    5 (+asciinema)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/MiriyamBird/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/MiriyamBird/frontend-project-lvl1/actions)
 <a href="https://codeclimate.com/github/MiriyamBird/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/35e472af33517ab7d6ff/maintainability" /></a>
+<a href="https://asciinema.org/a/Ll5LXwqnokzKZhy9FTdiIFsEn" target="_blank"><img src="https://asciinema.org/a/Ll5LXwqnokzKZhy9FTdiIFsEn.svg" /></a>
--- src/even.js
+++ src/even.js
@@ -1,43 +1,40 @@
 import readlineSync from 'readline-sync';
 
-const playGame = () => {   
+const playGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let showCongratulations = 0;
 
-    let showCongratulations = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.round(Math.random() * 100);
 
-    for (let i = 0; i < 3; i += 1) {
+    const correctAnswer = () => {
+      if (randomNumber % 2 === 0) {
+        return 'yes';
+      }
+      return 'no';
+    };
 
-        const randomNumber = Math.round(Math.random()*100);
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
 
-        const correctAnswer = () => {
-            if (randomNumber % 2 === 0) {
-                return 'yes';
-            } else {
-                return 'no';
-            }
-        };
+    if (userAnswer === correctAnswer()) {
+      console.log('Correct!');
+      showCongratulations += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer()}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
 
-        console.log(`Question: ${randomNumber}`);
-        const userAnswer = readlineSync.question('Your answer: ');
-    
-        if (userAnswer === correctAnswer()) {
-            console.log('Correct!');
-            showCongratulations += 1;
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer()}'.`);
-            console.log(`Let's try again, ${userName}!`);
-            break;
-        }
-        
-        if (showCongratulations === 3) {
-            console.log(`Congratulations, ${userName}!`);
-        }
-    }   
-}
+    if (showCongratulations === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+};
 
 export default playGame;

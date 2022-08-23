fdb860349b1658df7c13e95da711ff9d4808b37d
Miriyam Bird <miriyam_bird@icloud.com>
2022-08-23 15:24:18 +0300

    fix lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const playGame = (rules, startRound) => {
+const playGame = (rules, startRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
@@ -29,3 +29,5 @@ export const playGame = (rules, startRound) => {
     }
   }
 };
+
+export {playGame};
--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,8 @@ const playPrimeGame = () => {
   const startRound = () => {
     const getRandomNumber = () => Math.round(Math.random() * 100);
     const primeNumbers = [
-      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+      43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
     ];
 
     const randomNumber = getRandomNumber();

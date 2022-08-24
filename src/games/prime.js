ea2e5aea8c656d5526631edff77f135cce12f760
Miriyam Bird <miriyam_bird@icloud.com>
2022-08-24 11:15:37 +0300

    fix CodeClimate

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,28 +4,19 @@ const playPrimeGame = () => {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
   const startRound = () => {
-    const getRandomNumber = () => Math.round(Math.random() * 100);
     const primeNumbers = [
       2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
       43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
     ];
-
-    const randomNumber = getRandomNumber();
+    const randomNumber = Math.round(Math.random() * 100);
 
     const question = randomNumber;
 
-    const comparison = () => {
+    const correctAnswer = () => {
       for (let i = 0; i < primeNumbers.length; i += 1) {
         if (randomNumber === primeNumbers[i]) {
-          return true;
+          return 'yes';
         }
-      }
-      return false;
-    };
-
-    const correctAnswer = () => {
-      if (comparison() === true) {
-        return 'yes';
       }
       return 'no';
     };
